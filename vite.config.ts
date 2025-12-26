35d3d3e30050835575680294717bb14a4c56c6af
gkilon <gkilon@gmail.com>
2025-12-26 02:53:52 +0200

    ׳

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,6 +6,7 @@ export default defineConfig(({ mode }) => {
   // Use any cast to satisfy TypeScript when accessing Node process methods in Vite config
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
+    base: './',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY || process.env.API_KEY),
